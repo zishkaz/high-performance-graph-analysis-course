c883fb49b2b1e8567e2d36e6032e90a57e7aea51
Sergey Loktev <loktevsergay.2003@gmail.com>
2023-03-22 23:27:40 +0300

    Implemented multiple source BFS

--- project/bfs.py
+++ project/bfs.py
@@ -26,3 +26,33 @@ def bfs(graph: Matrix, source: int) -> List[int]:
     res.assign_scalar(-1, mask=res, desc=S & C)
 
     return list(res.vals)
+
+
+def ms_bfs(graph: Matrix, sources: List) -> List:
+    """
+    Performs BFS of a directed graph as matrix starting from the multiple source vertices
+
+    :param graph: graph as Matrix
+    :param sources: source vertices as ints
+    :return: list: source, list of parents for the shortest path of each node; source = -1, unreachable nodes = -2
+    """
+    n = graph.ncols
+    m = len(sources)
+
+    parents = Matrix.sparse(INT64, nrows=m, ncols=n)
+    front = Matrix.sparse(INT64, nrows=m, ncols=n)
+
+    for r, s in enumerate(sources):
+        front[r, s] = -1
+
+    while front.nvals:
+        parents.assign_matrix(value=front, mask=front, desc=S)
+        front.apply(INT64.POSITIONJ, out=front)
+        front.mxm(
+            other=graph, semiring=INT64.MIN_FIRST, out=front, mask=parents, desc=RSC
+        )
+
+    return [
+        [v, [parents.get(i, j, default=-2) for j in range(n)]]
+        for i, v in enumerate(sources)
+    ]
